23dcfb5475ef08bb21edd59f1ece4914703afdc8
Nikita Profir <neicker44536@gmail.com>
2024-01-30 18:20:08 +0300

    send message to be

--- fullStack/back-end/services/Mail.py
+++ fullStack/back-end/services/Mail.py
@@ -1,4 +1,6 @@
 import smtplib
+import ssl
+from email.message import EmailMessage
 
 
 def get_smtp_obj():
@@ -11,3 +13,26 @@ def get_smtp_obj():
     finally:
         smtp_obj.quit()
 
+
+def send_message_ibd(message: str, email: str):
+    smtp_server = "smtp.gmail.com"
+    port = 465   # используйте порт 465 для SSL 587
+    # server = smtplib.SMTP(smtp_server, port, )
+    # server.starttls()
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = 'Subject line'
+    msg['From'] = 'IBDCorporation31@gmail.com'
+    msg['To'] = 'toporov.axeman@gmail.com'
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls(context=context)  # Устанавливаем защищенное соединение
+        server.login('IBDCorporation31@gmail.com', 'Ee123123Ee')
+        server.send_message(msg)
+
+    # server.login('IBDCorporation31@gmail.com', 'Ee123123Ee')
+    #
+    # server.sendmail('ibdcorporation31@gmail.com', 'toporov.axeman@gmail.com', 'Аджарагуджу from IBDCorparation kuuurvaaaa')
+    #
+    # server.quit()
