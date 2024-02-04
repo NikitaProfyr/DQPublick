39cda9db8573bf97686d29147626b932e6af34e8
Toporov Denis <toporov.den2@list.ru>
2024-02-04 23:42:49 +0300

    fixed admin panel

--- fullStack/back-end/main.py
+++ fullStack/back-end/main.py
@@ -1,3 +1,5 @@
+from http.client import HTTPException
+
 from fastapi import FastAPI, Request, Depends
 from fastapi_cache.backends.redis import RedisBackend
 
@@ -6,7 +8,9 @@ from fastapi_cache import FastAPICache
 from redis import asyncio as aioredis
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+from starlette.status import HTTP_400_BAD_REQUEST
 
+from model.UserSchema import UserCreate
 from routers.UserRouter import user_public_router, user_private_router
 from routers.QuizRouter import quiz_private_router, quiz_public_router
 from dotenv import load_dotenv
@@ -15,6 +19,7 @@ from os import getenv
 from fastapi_pagination import add_pagination
 
 from security import SECRET_KEY
+from services.User import authenticated, create_token
 
 load_dotenv()
 
@@ -88,9 +93,19 @@ class AdminAuth(AuthenticationBackend):
         form = await request.form()
         username, password = form["username"], form["password"]
 
+        user = authenticated(db=db, user_data=UserCreate(userName=username, password=password))
+        if not user or user.isAdmin == False:
+            return False
+            # raise HTTPException(
+            #     status_code=HTTP_400_BAD_REQUEST,
+            #     detail="Некорректное имя пользователя или пароль",
+            #     headers={"WWW-Authenticate": "Bearer"},
+            # )
+
+        access_token = create_token(data={"userName": user.userName})
         # Validate username/password credentials
         # And update session
-        request.session.update({"token": "..."})
+        request.session.update({"token": access_token})
 
         return True
 
